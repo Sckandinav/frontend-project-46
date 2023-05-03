62096af6aee3a7d1a0efecc240892581a90d9441
Vasili <ballov_v@mail.ru>
2023-05-03 21:46:49 +0300

    make 4 task

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -2,26 +2,17 @@ import { test, expect } from '@jest/globals';
 // import fs from 'fs';
 import compare from '../src/compare.js';
 
-const file1 = `{
-  "host": "hexlet.io",
-  "timeout": 50,
-  "proxy": "123.234.53.22",
-  "follow": false
-}`;
+const file1 = '__fixtures__/file1.json';
 
-const file2 = `{
-  "timeout": 20,
-  "verbose": true,
-  "host": "hexlet.io"
-}`;
+const file2 = '__fixtures__/file2.json';
 
 test('myTest', () => {
   expect(compare(file1, file2)).toEqual(`{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-  }`);
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`);
 });
--- src/compare.js
+++ src/compare.js
@@ -2,11 +2,9 @@ import { readFileSync } from 'fs';
 import { getParse } from './parses.js';
 import compareTree from './buildDiffTrees.js';
 
-const readFile = (path) => readFileSync(path, 'utf8');
-
 const compare = (filepath1, filepath2) => {
-  const data1 = readFile(filepath1);
-  const data2 = readFile(filepath2);
+  const data1 = readFileSync(filepath1, 'utf8');
+  const data2 = readFileSync(filepath2, 'utf8');
   const dataParse1 = getParse(data1);
   const dataParse2 = getParse(data2);
   const data = compareTree(dataParse1, dataParse2);
