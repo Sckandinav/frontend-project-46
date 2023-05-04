f77f3955a9cbf4621dc4a8057563b1ff48359e1e
Vasili <ballov_v@mail.ru>
2023-05-04 11:51:38 +0300

    fixed tests

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,18 +1,21 @@
-import { test, expect } from '@jest/globals';
-// import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
 import compare from '../src/compare.js';
 
-const file1 = '__fixtures__/file1.json';
 
-const file2 = '__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename)
+const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const jsonResult = readFixture('expected_file.txt');
+
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
 
 test('myTest', () => {
-  expect(compare(file1, file2)).toEqual(`{
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`);
+  expect(compare(file1, file2)).toEqual(jsonResult);
 });
