8ed5320c92bffb5df16ea8899a5ca03f0a6a9731
Vasili <ballov_v@mail.ru>
2023-05-04 12:01:35 +0300

    fixed tests

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,14 +1,13 @@
+import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'node:path';
 import { readFileSync } from 'node:fs';
 import compare from '../src/compare.js';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename)
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const jsonResult = readFixture('expected_file.txt');
